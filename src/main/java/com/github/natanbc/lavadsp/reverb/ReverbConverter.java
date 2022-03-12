ac7ff32f2f4138427572dd6eaa422da340675cf6
Dalibor Trampota <dalibor.trampota@gmail.com>
2022-03-12 17:08:22 +0100

    Update ReverbConverter.java

--- src/main/java/com/github/natanbc/lavadsp/reverb/ReverbConverter.java
+++ src/main/java/com/github/natanbc/lavadsp/reverb/ReverbConverter.java
@@ -28,7 +28,7 @@ public class ReverbConverter {
 	public void setDecay(float decay) {
         this.decay = decay;
     }
-	
+
 	public void setMixPercent(float mixPercent) {
         this.mixPercent = mixPercent * 100;
     }
@@ -104,8 +104,8 @@ public class ReverbConverter {
 		float[] allPassFilterInput = new float[samples];
 		float decay = 0.131f;
 
-		println(delaySamples.toString() + " delay");
-		println(allPassFilterInput.length.toString() + " length");
+		println(Integer.toString(delaySamples) + " delay");
+		println(Integer.toString(allPassFilterInput.length) + " length");
 
 		//Applying algorithm for All Pass Filter
 		for(int i = 0; i < samples; ++i){
